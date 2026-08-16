a16265b063463418273e8d26b536316b8c2a069c
sidhantabehera77500-cmyk <sidhantabehera77500@gmail.com>
2026-08-16 13:27:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ async def send_request(encrypted_uid, token, url):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB53"
+            "ReleaseVersion": "OB54"
         }
         async with aiohttp.ClientSession() as session:
             async with session.post(url, data=edata, headers=headers) as response:
@@ -145,7 +145,7 @@ def make_request(encrypt, region, token):
             "Expect": "100-continue",
             "X-Unity-Version": "2018.4.11f1",
             "X-GA": "v1 1",
-            "ReleaseVersion": "OB53"
+            "ReleaseVersion": "OB54"
         }
         response = requests.post(url, data=edata, headers=headers, verify=False)
         binary = response.content
